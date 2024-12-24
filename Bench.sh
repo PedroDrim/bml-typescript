d0963896b991efb1b6461abd5f93efbf473588f5
pedro <pedrohenriquedrim@gmail.com>
2024-12-23 23:24:32 -0300

    corrigindo conjunto de valores da simulacao

--- Bench.sh
+++ Bench.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-bun start 1000
-bun start 10000
 bun start 100000
+bun start 200000
+bun start 300000
+bun start 400000
+bun start 500000
+bun start 600000
+bun start 700000
+bun start 800000
+bun start 900000
 bun start 1000000
