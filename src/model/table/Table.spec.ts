a6161a93ac11bcbc707ede7f840ff28eec185631
pedro <pedrohenriquedrim@gmail.com>
2024-12-27 23:42:12 -0300

    correcao de caso de teste

--- src/model/table/Table.spec.ts
+++ src/model/table/Table.spec.ts
@@ -1,14 +1,16 @@
 import { expect, test, describe } from "bun:test";
 import { Table } from "./Table";
+import type { UserInfo } from "../user-info/UserInfo";
 
 describe("Table", () => {
     test("1. Devera ser inscanciavel caso possua valor valido", () => {
         expect(new Table("./data/test.csv")).toBeTruthy()
     });
 
-    test("2. 'userInfoList' devera retornar valor corretamente", () => {
+    test("2. 'userInfoList' devera retornar valor corretamente", async () => {
         const mock: Table = new Table("./data/test.csv")
-        expect(mock.userInfoList).toBeTruthy()
+        const list: UserInfo[] = await mock.userInfoList
+        expect(list.length).toEqual(10)
     });
 });
 
